e675b3666de2b10e1b64109d1f4a15c1ba4ae6f4
cruz-torres-dev <torresramses2005@gmail.com>
2026-02-14 18:44:26 -0600

    Update test_parser.py

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,1 +1,14 @@
+import pytest
+from src.fixed.parser import get_user_id
+
+def test_get_user_id_ok():
+    assert get_user_id('{"user":{"id":"12"}}') == 12
+
+def test_get_user_id_invalid_json():
+    with pytest.raises(Exception):
+        get_user_id("{bad json}")
+
+def test_get_user_id_missing_key():
+    with pytest.raises(Exception):
+        get_user_id('{"foo": 1}')
 
