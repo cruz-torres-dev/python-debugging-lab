26491f661cc9984d907fbd095930a46c91a4aa07
cruz-torres-dev <torresramses2005@gmail.com>
2026-02-14 18:45:01 -0600

    Update test_inventory.py

--- tests/test_inventory.py
+++ tests/test_inventory.py
@@ -1,1 +1,8 @@
+from src.fixed.inventory import count_items
+
+def test_count_items_ok():
+    assert count_items(["a", "b", "a"]) == {"a": 2, "b": 1}
+
+def test_count_items_empty():
+    assert count_items([]) == {}
 
